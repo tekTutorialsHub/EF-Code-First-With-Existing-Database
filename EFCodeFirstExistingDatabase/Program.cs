e80e2c7918c5ea4819fe436e3b66337576733d1b
tekTuturialsHub <admin@tekTutorialsHub.com>
2016-07-30 18:21:44 +0530

    Add project files.

---
+++ EFCodeFirstExistingDatabase/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstExistingDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new models.AdventureWorks())
+            {
+                var People = ctx.People.ToList();
+
+                foreach (var person in People)
+                {
+                    Console.WriteLine(string.Format("{0} {1}",
+                        person.FirstName,
+                        person.LastName));
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
